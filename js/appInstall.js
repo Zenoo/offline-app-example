7663b746bdb5035b8f9c5709513e9646c660441b
Zenoo <fzenooo@gmail.com>
2018-12-07 09:27:23 +0100

    Added an 'update app' button

--- js/appInstall.js
+++ js/appInstall.js
@@ -1,22 +1,22 @@
 let deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', e => {
-	// Prevent Chrome 67 and earlier from automatically showing the prompt
 	e.preventDefault();
-	// Stash the event so it can be triggered later.
+
 	deferredPrompt = e;
 	document.querySelector('.add-app').classList.add('shown');
 });
 
 window.addEventListener('load', () => {
-	const installButton = document.querySelector('.add-app button');
+	const 
+		installButton = document.querySelector('.add-app button'),
+		updateButton = document.querySelector('.update-app button');
 
+	// Install app
 	installButton.addEventListener('click', () => {
-		// hide our user interface that shows our A2HS button
 		installButton.parentElement.classList.remove('shown');
-		// Show the prompt
+
 		deferredPrompt.prompt();
-		// Wait for the user to respond to the prompt
 		deferredPrompt.userChoice
 			.then((choiceResult) => {
 				if (choiceResult.outcome === 'accepted') {
@@ -26,6 +26,15 @@ window.addEventListener('load', () => {
 				}
 				deferredPrompt = null;
 			});
+	});
+
+	// Update app
+	updateButton.addEventListener('click', () => {
+		let newURL = new URL(location);
+
+		newURL.searchParams.append('v', Math.random().toString(36).substring(7));
+
+		window.location.href = newURL.href;
 	});
 	
 	window.addEventListener('appinstalled', () => {
